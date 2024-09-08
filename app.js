d2253e9944d804ff4942e2332a5ec2494ba26d64
Joseilton <108443252+joseiltonfsn@users.noreply.github.com>
2024-09-07 23:49:03 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,58 @@
+function pesquisar(){
+  
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value;   
+
+    campoPesquisa = campoPesquisa.toLowerCase(); if(campoPesquisa.trim() === ""){
+        section.innerHTML = "Digite um texto para sua pesquisa!!"
+    return
+    }
+/*
+   
+    if(!campoPesquisa){
+        section.innerHTML = "Informação não encontrada!!"
+        return
+    }
+
+*/
+    let resultados = "";
+    let titulo = "";
+    let descricao = "";
+    let tags = "";
+
+    for( let dado  of dados){
+        titulo = dado.titulo.toLowerCase();
+        descricao = dado.descricao.toLowerCase();
+        tags = dado.tags.toLowerCase();
+
+        if(titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)){
+            console.log(dado.titulo.includes(campoPesquisa))
+
+        resultados += `<div class="item-resultado">
+            <h2>
+                <a href="#" target="_blank">
+                    ${dado.titulo}
+                </a>
+            </h2>
+            <p class="descricao-meta">${dado.descricao} 
+                <a href="${dado.link}" target="_blank"> 
+                    saiba mais...
+                </a>
+            </p>
+        </div>`;        
+        }  
+        /*      
+        else{
+            resultados = "Informação não consta na base de dados"
+        }
+        */
+    }
+
+    if(!resultados){
+    resultados = "Nada foi encontrado"
+    }
+    
+    section.innerHTML = resultados; 
+
+    }
---
+++ dados.js
@@ -0,0 +1,40 @@
+let dados = [
+    {
+      
+      titulo: "Smart Home by Setin Alto do Ipiranga São Paulo",
+      descricao: "Com uma gama extensa de áreas comuns - desde piscinas e quadra até espaço gourmet e salão de festas, seus momentos de descanso serão repletos de diversão e relaxamento, promovendo integração e satisfação para toda a família.",
+      link: "https://www.setin.com.br/smart-home-alto-do-ipiranga/?gad_source=1&gclid=Cj0KCQjw8--2BhCHARIsAF_w1gyBQmW69t7W63oAo8f5CxQuq1oBN5Adeuc5bJzHOfg-QI-yICuIPbwaAjSaEALw_wcB",
+      tags: "apartamento sala dormitorio dormitório cozinha wc banheiro toilet gerador carregador carro eletrico poluentes lockers metro coworking fitness lavanderia"
+    },
+    {
+      titulo: "Residencial Évora Santo André",
+      descricao: "Apresentamos, o Residencial Évora, novo lançamento da Recon. Apartamentos amplos e aconchegantes, com 55 m² de espaço, dois dormitórios, uma suíte, varanda grill, uma vaga de garagem, depósito privativo e lazer completo!",
+      link: "https://www.reconeng.com.br/evora?gad_source=1&gclid=Cj0KCQjw8--2BhCHARIsAF_w1gx8sdGhjn4nT6iDrBue2nvSaJutQAsUL0xArcBjA_CasHeckxKE4nYaAsr_EALw_wcB://cury.net/sobre-a-cury/institucional",
+      tags: "apartamento sala dormitorio dormitório cozinha wc banheiro toilet comércios comercios évora escolas parques comodidades financiamento caixa andre "
+    },
+    {
+      titulo: "Braido Santa Paula São Caetano do Sul",
+      descricao: "A Braido está trazendo mais um projeto de primeiro linha para São Caetano do Sul, em um dos bairros mais desejados da cidade: Santa Paula, a menos de 50m da Av Goias. Trata-se de um ponto muito arborizado e com ótima infraestrutura. Torre Única com 13 andares, sendo 4 unidades por andar com apartamentos com 104m², 3 suítes, varanda gourmet com uma churrasqueira a carvão, lavabo, ventilação natural nos banheiros, 2 vagas e muito mais!",
+      link: "https://www.lancamentobraido.com.br/?gad_source=1&gclid=Cj0KCQjw8--2BhCHARIsAF_w1gweRRtxc-KAQYR9oSMLabSZRDLu5udblIknM5UW8yM-SNG2KXHNxTMaAm4-EALw_wcB",
+      tags: "apartamento sala dormitorio dormitório cozinha wc banheiro toilet sao piscina quadra areia academia fitness coworking festas jogos salão salao beleza beauty care"
+    },
+    {
+      titulo: "Vert MBigucci Aclimação São Paulo ",
+      descricao: "Vert foi criado pela MBigucci para verticalizar o seu grande sonho, levando-o para o alto até o apartamento que a sua família tanto deseja.É o conceito de bem-estar em uma posição superior, acima do lugar comum. Está muito próximo da Liberdade, SP e ao Parque da Aclimação, que traduz tão bem o conceito de vida saudável ao ar livre. No espaço interno, conforto e bom gosto convivem em harmonia. ",
+      link: "https://www.mbigucci.com.br/vert/?utm_source=search_vert&utm_medium=cpc&utm_campaign=br-search_vert-mb_vert-search_vert-mql&utm_source=&utm_medium=&utm_campaign=&utm_content=&gad_source=1&gclid=Cj0KCQjw8--2BhCHARIsAF_w1gyV3XpNvvWsbC94e8gvSji1m2ezOtzN6bDqI6LKOVvB5kiRVtKkdOYaAs6xEALw_wcB ",
+      tags: "apartamento sala dormitorio dormitório cozinha wc banheiro toilet aclimacao sao liberdade bueno andrade lixeira coworking pet place vestiarios fitness playground festas "
+    },
+    {
+      titulo: " Douro Residencial Clube Bairro Jardim Santo André  ",
+      descricao: " Inspirado na pitoresca região vinícola ao norte de Portugal, o Douro Residencial Clube oferece o equilíbrio perfeito entre bem-estar e comodidade em um dos endereços mais desejados de Santo André. ",
+      link: " https://exkalla.com.br/lp-douro/?gad_source=1&gclid=Cj0KCQjw8--2BhCHARIsAF_w1gz6v3mYkOzDUvfexLtePoAiYQOixNctbG_Am3tqPK8xyqTsCPBf42saAve8EALw_wcB# ",
+      tags: "apartamento sala dormitorio dormitório cozinha wc banheiro toilet andre deck molhado solarium spa playground salão salao beleza beauty care arena jogos festas piscina market conveniencia pay per use pet place"
+    },
+    {
+      titulo: "Kiruna Patriani São Caetano ",
+      descricao: "Se você quer viver em um ambiente moderno e bem equipado, o KIRUNA PATRIANI, em São Caetano, é a melhor opção! O KIRUNA PATRIANI é um dos prédios mais modernos e tecnológicos da cidade. Cada apartamento terá direito a 2 vagas, sendo uma box que acomoda  SUV, além de ser  fechada nas laterais, e outra com infraestrutura elétrica. São 90 unidades de 82 a 86 m² com 2 suítes e lavabo que serão entregues com porcelanato no apartamento inteiro. Além disso, box nos banheiros e aquecedor a gás instalado no quintal ",
+      link: "https://www.construtorapatriani.com.br/imovel/kiruna-patriani#o-predio ",
+      tags: "apartamento sala dormitorio dormitório cozinha wc banheiro toilet sao salão salao beleza beauty care pet place carregador carro eletrico piscina fitness   "
+    }
+  ];
+  
